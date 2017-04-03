c29f4f8568c9f829cfcafb874dc15559f887c2f5
Peter Rybar <pr.rybar@gmail.com>
2017-04-03 20:21:22 +0200

    rollup config update

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 import typescript from 'rollup-plugin-typescript';
 import replace from 'rollup-plugin-replace'
+// import babel   from 'rollup-plugin-babel'
+// import es2015 from 'babel-preset-es2015-rollup';
 import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-js';
 
 const pkg = require('./package.json');
 
@@ -13,7 +16,10 @@ export default {
         replace({
             '@VERSION@': pkg.version
         }),
-        uglify()
+        // babel({
+        //     presets: [es2015]
+        // }),
+        uglify({}, minify)
     ],
     globals: {
         // 'jquery': 'jQuery'
