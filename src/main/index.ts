065f933fa733cf32daaff4ce8c70162cc6ca082b
Peter Rybar <pr.rybar@gmail.com>
2017-04-10 13:58:00 +0200

    jsonml templates

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,6 @@
 
 import { HttpRequest } from "./prest/http";
-import { Widget, html, select } from "./prest/dom";
+import { Widget, html, select, jsonml } from "./prest/dom";
 import { Signal } from "./prest/signal";
 
 export const version: string = "@VERSION@";
@@ -65,18 +65,37 @@ class ProductWidget implements Widget {
         if (this._element) {
             this._element.innerHTML = "";
             const p = this._product;
-            const el = html(`
-                <div class="product" title="id: ${p.id}">
-                    ${p.title}
-                    <strong>${p.price} €</strong>
-                    <em>${p.count} ks</em>
-                    <input type="number" value="1" min="1" max="${p.count}" step="1"/>
-                    <button type="button">roder</button>
-                </div>`);
-            const i = select("input", el) as HTMLInputElement;
-            select("button", el).addEventListener("click", () => {
-                this.sigOrder.emit({ product: this._product, count: +i.value });
-            });
+            // const el = html(`
+            //     <div class="product" title="id: ${p.id}">
+            //         ${p.title}
+            //         <strong>${p.price} €</strong>
+            //         <em>${p.count} ks</em>
+            //         <input type="number" value="1" min="1" max="${p.count}" step="1"/>
+            //         <button type="button">roder</button>
+            //     </div>`);
+            // const i = select("input", el) as HTMLInputElement;
+            // select("button", el).addEventListener("click", () => {
+            //     this.sigOrder.emit({
+            //         product: this._product,
+            //         count: +i.value
+            //     });
+            // });
+            const el = jsonml(
+                ["div.product", { title: `id: ${p.id}`},
+                    p.title, " ",
+                    ["strong", p.price, " €"], " ",
+                    ["em", p.count, " ks"], " ",
+                    ["input", { type: "number", value: 1, min: 1, max: p.count, step: 1 }], " ",
+                    ["button", { type: "button",
+                            click: () => {
+                                const i = select("input", el) as HTMLInputElement;
+                                this.sigOrder.emit({
+                                    product: this._product,
+                                    count: +i.value
+                                });
+                            }
+                        },
+                        "roder"]]);
             this._element.appendChild(el);
         }
     }
@@ -135,12 +154,15 @@ function main() {
                     console.log("order", order);
                     server.orderPost(order, o => {
                         console.log("order accepted", o);
-                        ordersElement.appendChild(html(`
-                            <div class="order" title="id: ${o.id}">
-                                ${p.title}:
-                                ${o.count} &times; ${o.product.price} € &equals; 
-                                <strong>${o.product.price * o.count} €</strong>
-                            </div>`));
+                        // ordersElement.appendChild(html(`
+                        //     <div class="order" title="id: ${o.id}">
+                        //         ${p.title}: ${o.count} &times; ${o.product.price} € =
+                        //         <strong>${o.product.price * o.count} €</strong>
+                        //     </div>`));
+                        ordersElement.appendChild(jsonml(
+                            ["div.order", { title: `id: ${o.id}` },
+                                `${p.title}: ${o.count} &times; ${o.product.price} € = `,
+                                ["strong", `${o.product.price * o.count} €`]]));
                     });
                 });
             productsElement.appendChild(pw.element());
